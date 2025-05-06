7fafc7f461621f6e727dff19226f62c6f04c9237
Josiah Mendez <89169757+Josiah-Mendez-CS@users.noreply.github.com>
2025-05-06 11:10:03 -0400

    Delete storage/framework/views directory

--- storage/framework/views/0d036f732c0b4189202ee8adeb4b1562.php
+++
@@ -1,3 +0,0 @@
-<label <?php echo e($attributes->merge([
-    'class' => 'form-label',
-])); ?>><?php echo e($slot); ?></label><?php /**PATH /var/www/resources/views/components/forms/input-label.blade.php ENDPATH**/ ?>
--- storage/framework/views/6b347a8685470432bca53a4f3c7885ea.php
+++
@@ -1,5 +0,0 @@
-<?php if( !app()->environment('production') ): ?>
-<div class="py-2 text-bg-warning text-center w-100">
-    <strong>Warning:</strong> You are in the <strong><?php echo e(app()->environment()); ?></strong> environment.
-</div>
-<?php endif; ?><?php /**PATH /var/www/resources/views/components/environment-banner.blade.php ENDPATH**/ ?>
